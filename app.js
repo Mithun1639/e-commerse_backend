33843d60b49f6e7ebdc2c0fa89e5c803228a7e32
Your Name <mithunreddy011@gmail.com>
2024-11-25 10:08:15 +0530

    4th commit

--- app.js
+++ app.js
@@ -12,21 +12,24 @@ const cookieParser = require("cookie-parser");
 app.use(express.json());
 app.use(cookieParser());
 // List of allowed origins
-const allowedOrigins = [
-  "http://localhost:3000",
-  "https://golden-queijadas-d9b5e0.netlify.app",
-];
-const corsOptions = {
-  origin: function (origin, callback) {
-    if (allowedOrigins.includes(origin) || !origin) {
-      callback(null, true);
-    } else {
-      callback(new Error("Not allowed by CORS"));
-    }
-  },credentials: true
-};
+// const allowedOrigins = [
+//   "http://localhost:3000",
+//   "https://golden-queijadas-d9b5e0.netlify.app",
+// ];
+var corsOptions = {
+  origin: 'http://localhost:3000',
+  credentials : true
+ }
 
 app.use(cors(corsOptions));
+
+app.use(function (req, res, next) {	
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');    
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');    
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');   
+    res.setHeader('Access-Control-Allow-Credentials', true);    
+    next();
+});
 app.use(router);
 app.use("/files", express.static("./public/files"));
 
